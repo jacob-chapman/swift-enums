4c43e31255ab00543af1de1f728f6ad145897474
Jacob Chapman <jacobchapman5@gmail.com>
2022-11-07 11:48:45 -0500

    bump version

--- swift-enums-plugin/src/main/kotlin/com/jacobchapman/swiftenums/plugin/SwiftEnumsPlugin.kt
+++ swift-enums-plugin/src/main/kotlin/com/jacobchapman/swiftenums/plugin/SwiftEnumsPlugin.kt
@@ -35,7 +35,7 @@ class SwiftEnumsPlugin : Plugin<Project> {
             extensions.create("swiftEnums", SwiftEnumsExtension::class.java)
             kotlin {
                 this.sourceSets.getByName("commonMain").dependencies {
-                    implementation("io.jacobchapman:swift-enums-core:0.0.3")
+                    implementation("io.jacobchapman:swift-enums:swift-enums-core:0.0.5")
                 }
             }
 
@@ -46,7 +46,7 @@ class SwiftEnumsPlugin : Plugin<Project> {
                     configurations.filter { it.name.contains("ksp") && it.name.contains("ios", true) }.forEach {
 //                        println("[SwiftEnums] Adding Ksp: $it")
                         dependentKspTasks.add(it.name)
-                        add(it.name, "io.jacobchapman:swift-enums-processor:0.0.3")
+                        add(it.name, "io.jacobchapman:swift-enums:swift-enums-processor:0.0.5")
                     }
                 }
                 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
